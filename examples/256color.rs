f8fdcbeb2b1250ab4e5c80263d9ec6eee6981298
Jinzhou Zhang <lotabout@gmail.com>
2019-01-05 17:45:29 +0800

    [color] add examples for dealing with colors

---
+++ examples/256color.rs
@@ -0,0 +1,31 @@
+use std::io;
+use tuikit::attr::Color;
+use tuikit::output::Output;
+
+fn main() {
+    let mut output = Output::new(io::stdout()).unwrap();
+
+    for fg in 0..=255 {
+        output.set_fg(Color::AnsiValue(fg));
+        output.write(format!("{:5}", fg).as_str());
+        if fg % 16 == 15 {
+            output.reset_attributes();
+            output.write("\n");
+            output.flush()
+        }
+    }
+
+    output.reset_attributes();
+
+    for bg in 0..=255 {
+        output.set_bg(Color::AnsiValue(bg));
+        output.write(format!("{:5}", bg).as_str());
+        if bg % 16 == 15 {
+            output.reset_attributes();
+            output.write("\n");
+            output.flush()
+        }
+    }
+
+    output.flush()
+}
---
+++ examples/true_color.rs
@@ -0,0 +1,80 @@
+use std::io;
+use tuikit::attr::Color;
+use tuikit::output::Output;
+
+// ported from: https://github.com/gnachman/iTerm2/blob/master/tests/24-bit-color.sh
+// should be run in terminals that supports true color
+
+// given a color idx/22 along HSV, return (r, g, b)
+fn rainbow_color(idx: u8) -> (u8, u8, u8) {
+    let h = idx / 43;
+    let f = idx - 43 * h;
+    let t = ((f as i32 * 255) / 43) as u8;
+    let q = 255 - t;
+
+    match h {
+        0 => (255, t, 0),
+        1 => (q, 255, 0),
+        2 => (0, 255, t),
+        3 => (0, q, 255),
+        4 => (t, 0, 255),
+        5 => (255, 0, q),
+        _ => unreachable!(),
+    }
+}
+
+fn try_background(output: &mut Output, r: u8, g: u8, b: u8) {
+    output.set_bg(Color::Rgb(r, g, b));
+    output.write(" ")
+}
+
+fn reset_output(output: &mut Output) {
+    output.reset_attributes();
+    output.write("\n");
+    output.flush();
+}
+
+fn main() {
+    let mut output = Output::new(io::stdout()).unwrap();
+    for i in 0..=127 {
+        try_background(&mut output, i, 0, 0);
+    }
+    reset_output(&mut output);
+
+    for i in (128..=255).rev() {
+        try_background(&mut output, i, 0, 0);
+    }
+    reset_output(&mut output);
+
+    for i in 0..=127 {
+        try_background(&mut output, 0, i, 0);
+    }
+    reset_output(&mut output);
+
+    for i in (128..=255).rev() {
+        try_background(&mut output, 0, i, 0);
+    }
+    reset_output(&mut output);
+
+    for i in 0..=127 {
+        try_background(&mut output, 0, 0, i);
+    }
+    reset_output(&mut output);
+
+    for i in (128..=255).rev() {
+        try_background(&mut output, 0, 0, i);
+    }
+    reset_output(&mut output);
+
+    for i in 0..=127 {
+        let (r, g, b) = rainbow_color(i);
+        try_background(&mut output, r, g, b);
+    }
+    reset_output(&mut output);
+
+    for i in (128..=255).rev() {
+        let (r, g, b) = rainbow_color(i);
+        try_background(&mut output, r, g, b);
+    }
+    reset_output(&mut output);
+}
