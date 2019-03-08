da37a308cc5139d0283fa194cafaa139310f4698
Jinzhou Zhang <lotabout@gmail.com>
2019-03-08 08:25:07 +0800

    tmp commit

--- src/container.rs
+++ src/container.rs
@@ -1,14 +1,36 @@
 ///! Various pre-defined container that implements Draw
 
 use crate::draw::Draw;
-use crate::canvas::Canvas;
+use crate::canvas::{Canvas, Result};
 use crate::cell::Cell;
+use derive_builder::Builder;
 
-pub struct Window {
-
+#[derive(Debug, Copy, Clone)]
+pub enum Size {
+    Fixed(usize),
+    Percent(usize),
 }
 
-impl Draw for Window {
+///! A box is like a div in HTML, it has its margin/padding, and border
+#[derive(Builder, Debug)]
+pub struct Box {
+    margin_top: Size,
+    margin_right: Size,
+    margin_bottom: Size,
+    margin_left: Size,
+
+    padding_top: Size,
+    padding_right: Size,
+    padding_bottom: Size,
+    padding_left: Size,
+
+    border_top: bool,
+    border_right: bool,
+    border_bottom: bool,
+    border_left: bool,
+}
+
+impl Draw for Box {
     fn draw(&self, canvas: &mut Canvas) {
         unimplemented!()
     }
@@ -39,23 +61,23 @@ struct BoundedCanvas<'a, T: Canvas> {
 }
 
 impl<'a, T:Canvas> Canvas for BoundedCanvas<'a, T> {
-    fn size(&self) -> _ {
+    fn size(&self) -> Result<(usize, usize)> {
         unimplemented!()
     }
 
-    fn clear(&mut self) -> _ {
+    fn clear(&mut self) -> Result<()> {
         unimplemented!()
     }
 
-    fn put_cell(&mut self, row: usize, col: usize, cell: Cell) -> _ {
+    fn put_cell(&mut self, row: usize, col: usize, cell: Cell) -> Result<()> {
         unimplemented!()
     }
 
-    fn set_cursor(&mut self, row: usize, col: usize) -> _ {
+    fn set_cursor(&mut self, row: usize, col: usize) -> Result<()> {
         unimplemented!()
     }
 
-    fn show_cursor(&mut self, show: bool) -> _ {
+    fn show_cursor(&mut self, show: bool) -> Result<()> {
         unimplemented!()
     }
 }
