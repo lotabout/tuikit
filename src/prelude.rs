f745ad63a2145bb2a48c5bfcbe3aaf2cd8733b94
Jinzhou Zhang <lotabout@gmail.com>
2020-01-30 12:11:49 +0800

    export Rectangle & MouseButton to prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,7 +1,8 @@
 pub use crate::attr::{Attr, Color, Effect};
 pub use crate::canvas::{Canvas, Result};
 pub use crate::cell::Cell;
-pub use crate::widget::{HSplit, Size, Split, VSplit, Widget, Win};
+pub use crate::widget::{HSplit, Size, Split, VSplit, Widget, Win, Rectangle};
 pub use crate::draw::Draw;
-pub use crate::event::{Event, Key};
+pub use crate::event::Event;
+pub use crate::key::*;
 pub use crate::term::{Term, TermHeight, TermOptions};
